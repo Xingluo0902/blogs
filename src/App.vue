1b3828982c8db39491f83ac23823664694b89fdf
Petrichor-02 <3391162147@qq.com>
2021-07-23 16:41:20 +0800

    创建项目--第一次提交

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,7 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </div>
-  <router-view/>
+<div>
+  <h1>blogs</h1>
+</div>
 </template>
 
 <style>
